2b5e96cd34c59169d533f2199da33e518d89f2b3
nabe-del <businessnabe@gmail.com>
2025-05-15 22:13:31 +0900

    test

--- woonuxt_base/app/pages/categories.vue
+++ woonuxt_base/app/pages/categories.vue
@@ -15,5 +15,4 @@ useHead({
       <CategoryCard v-for="(category, i) in productCategories" :key="i" :node="category" :image-loading="i <= 2 ? 'eager' : 'lazy'" />
     </div>
   </main>
-  <!-- test -->
 </template>
